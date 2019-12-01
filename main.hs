ff59320abbf65f8d9d331a9f7464bb0606af9e53
Yasuaki Kudo <yasu@yasuaki.com>
2019-12-01 09:09:06 +0000

    test

--- main.hs
+++ main.hs
@@ -1,1 +1,12 @@
 main = putStrLn("3")
+
+cp :: [[a]] -> [[a]]
+cp [] = [[]]
+cp (xs:xss) = [x:ys | x <- xs, ys <- yss]
+            where 
+            yss = cp xss
+
+
+cp2 [xs] = cp (xs:[])
+
+s = [x:y | x <- [1,2,3], y <- []]
